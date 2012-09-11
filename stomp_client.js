f64f53e8eb0e2be9e62ab849fcc10fa0479a7d38
Jonathan Pollock <jpollock@moxiesoft.com>
2012-09-11 10:44:54 -0400

    Moved autonomy logic into stomp_client file to make it easier to do aborts and quickly wind down index requests.

--- stomp_client.js
+++ stomp_client.js
@@ -3,6 +3,13 @@ var stomp = require('stomp');
 var os = require('os');
 var dateFormat = require('dateformat');
 
+var http = require('http');
+var knox = require('knox');
+var path = require('path');
+var querystring = require('querystring');
+
+var process_state = "RUNNING";
+
 // read preferences json file from disk
 var pref_file = fs.readFileSync(__dirname + '/stomp_preferences.json', 'utf8');
 var pref = JSON.parse(pref_file);
@@ -15,6 +22,12 @@ var logger = new (winston.Logger)({
 });
 
 logger.add(winston.transports.Console, {timestamp: true});
+logger.add(winston.transports.File, {
+    filename: 'stomp_client.log',
+    timestamp: true,
+    maxFiles: 5,
+    maxsize: 10485760 // 10 MB
+});
 
 var graylogOpts = {
     graylogHost: pref.graylogHost,
@@ -26,25 +39,6 @@ var Graylog2 = require('winston-graylog2').Graylog2;
 logger.add(Graylog2, graylogOpts);
 
 
-StompLogging.prototype.debug = function(message) {
-    logger.debug(message);
-};
-
-StompLogging.prototype.warn = function(message) {
-    logger.warn(message);
-};
-
-StompLogging.prototype.error = function(message, die) {
-    logger.error(message);
-    if (die) {
-        process.exit(1);
-    }
-};
-
-var Autonomy = require('./autonomy');
-var autonomy = new Autonomy(pref.docs_dir);
-
-
 var stomp_args = {
     port: pref.port,
     host: pref.host,
@@ -54,6 +48,16 @@ var stomp_args = {
 };
 
 var client = new stomp.Stomp(stomp_args);
+
+client.log.debug = function(message) { logger.debug(message); };
+client.log.warn = function(message) { logger.warn(message); };
+client.log.error = function(message, die) {
+    logger.error(message);
+    if (die) {
+        process.exit(1);
+    }
+};
+
 
 var headers = {
     id: 1,
@@ -114,14 +118,10 @@ function filter_empty(str) {
     }
 }
 
-var messages_outstanding_count = 0;
-var msg_counter = 1;
-var messages = {};
+var messages = [];
 
 function MessageContext(message) {
-    this.msg_number = msg_counter++;
-    messages_outstanding_count++;
-    messages[this.msg_number] = this;
+    messages.push(this);
 
     this.message = message;
     this.startTime = new Date();
@@ -199,39 +199,196 @@ MessageContext.prototype.ack = function() {
     var endTime = new Date();
     var elapsed = (endTime.getTime() - this.startTime.getTime()) / 1000;
     client.ack(this.message_id);
-    this.removeMessage();
     this.send_stats(endTime, elapsed, "SUCCESS");
     this.log("ACK: " + this.message_id + " elapsed: " + elapsed + " seconds");
+    this.cleanup();
 };
 
 MessageContext.prototype.nack = function() {
     var endTime = new Date();
     var elapsed = (endTime.getTime() - this.startTime.getTime()) / 1000;
     client.ack(this.message_id);
-    this.removeMessage();
     this.send_stats(endTime, elapsed, "FAILED");
     this.log("NACK: " + this.message_id + " elapsed: " + elapsed + " seconds");
+    this.cleanup();
 };
 
 MessageContext.prototype.removeMessage = function() {
-    delete messages[this.msg_number];
-    messages_outstanding_count--;
+    var idx = messages.indexOf(this);
+    if (idx >= 0) {
+        messages.splice(idx, 1);
+    }
 };
 
+MessageContext.prototype.is_shutting_down = function() {
+    return (process_state === "RUNNING") ? false : true;
+};
+
+MessageContext.prototype.cleanup = function() {
+    var ctx = this;
+    if (ctx.path_to_file) {
+        fs.stat(ctx.path_to_file, function(err, stats) {
+            if (err) { return; }
+            if (stats.isFile()) {
+                fs.unlink(ctx.path_to_file);
+            }
+        });
+    }
+    this.removeMessage();
+    return true;
+};
+
+MessageContext.prototype.index = function () {
+    var ctx = this;
+
+    var key = ctx.json_data.s3_aws_key;
+    var secret = ctx.json_data.s3_aws_secret;
+    var bucket = ctx.json_data.s3_bucket;
+    var file_id = ctx.json_data.file_id;
+    var file_name = ctx.json_data.file_name;
+    var autonomy_db_name = ctx.json_data.autonomy_db_name;
+    var stubidx = ctx.json_data.stubidx;
+
+    var docs_dir = pref.docs_dir;
+
+    // create the s3 client
+    var knoxClient = knox.createClient({
+        key: key,
+        secret: secret,
+        bucket: bucket
+    });
+
+    ctx.log("submitting the s3 request to download the document");
+    var s3_path = file_id + '/' + querystring.escape(file_name);
+
+    // request the s3 document
+    ctx.s3_request = knoxClient.get(s3_path);
+    ctx.s3_request.on('error', function(err) {
+        if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+        ctx.log("error on s3 request: " + err);
+        ctx.nack();
+    });
+    ctx.s3_request.on('response', function (s3_res) {
+        if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+        ctx.s3_request = null;
+
+        ctx.log("downloading " + s3_path + " from s3...");
+        ctx.log("download status code: " + s3_res.statusCode);
+
+        // create a directory with the same name as the db on the autonomy server
+        var unique_name = (autonomy_db_name + "/" + file_id + "/" + file_name).replace(/\//g, "_");
+        ctx.path_to_file = docs_dir + unique_name;
+
+        // stream the document to disk chunk by chunk
+        var outstream = fs.createWriteStream(ctx.path_to_file);
+        s3_res.on('data', function (chunk) {
+            outstream.write(chunk);
+        });
+
+        // the file has been saved! now let's build the autonomy request
+        s3_res.on('end', function () {
+            if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+            ctx.log("Submitting the data to autonomy filesystemfetch");
+            outstream.end();
+            var xml = "<?xml version=\"1.0\"?><autn:import><autn:envelope><autn:stubidx><![CDATA[" + stubidx + "]]></autn:stubidx><autn:document><autn:fetch url=\"" + ctx.path_to_file + "\" deleteoriginal=\"true\" /></autn:document></autn:envelope></autn:import>";
+            ctx.log(xml);
+
+            var idol_data = querystring.stringify({
+                'Data': stubidx,
+                'DREDBNAME': autonomy_db_name,
+                'EnvelopeXML': xml,
+                'jobname': 'ImportEnvelopeJob',
+                'EnvelopeImportFailOnImport': 'never'
+            });
+
+            var http_options = {
+                host: 'localhost',
+                port: 7000,
+                path: '/action=ImportEnvelope',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': idol_data.length
+                }
+            };
+
+            // post to autonomy to index the document
+            ctx.index_req = http.request(http_options, function (res) {
+                res.setEncoding('utf8');
+                var autonomy_response = "";
+                res.on('data', function (chunk) {
+                    if (ctx.is_shutting_down()) {
+                        return;
+                    }
+                    autonomy_response = autonomy_response + chunk;
+                });
+                res.on('end', function() {
+                    if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+                    ctx.log('Autonomy Response: ' + autonomy_response);
+                    ctx.ack();
+                });
+                res.on('error', function (e) {
+                    ctx.log('Autonomy problem with request: ' + e.message);
+                    ctx.nack();
+                });
+            });
+
+            ctx.index_req.on('error', function(error) {
+                if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+                ctx.log('error trying to make autonomy request ' + error);
+                ctx.nack();
+            });
+
+            // post the data
+            ctx.index_req.write(idol_data);
+            ctx.index_req.end();
+        });
+    }).end();
+};
+
+MessageContext.prototype.unindex = function() {
+    var ctx = this;
+
+    // unique autonomy id
+    var reference = ctx.json_data.reference;
+
+    // each customer has their own autonomy db
+    var db = ctx.json_data.db;
+    ctx.log("unindex reference: " + reference + ", db: " + db);
+
+    var http_options = {
+        host: 'localhost',
+        port: 9001,
+        path: '/DREDELETEREF?Docs=' + reference + '&DREDbName=' + db
+    };
+
+    // post to autonomy to unindex the document
+    ctx.unindex_req = http.get(http_options, function (res) {
+        if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+        ctx.log("unindex response: " + res.statusCode);
+        ctx.ack();
+    }).on('error', function (e) {
+        if (ctx.is_shutting_down()) { return ctx.cleanup(); }
+        ctx.log("unindex error: " + e.message);
+        ctx.nack();
+    });
+};
 
 client.on('message', function(message) {
+    if (process_state !== "RUNNING") { return; }
+
     var ctx = new MessageContext(message);
     ctx.log("message_id: " + ctx.message_id + " body: " + ctx.body);
 
     if (ctx.action === "unindex") {
         try {
-            autonomy.unindex(ctx.json_data, ctx);
+            ctx.unindex();
         } catch (ex) {
             ctx.process_exception(ex);
         }
     } else if (ctx.action === "index") {
         try {
-            autonomy.index(ctx.json_data, ctx);
+            ctx.index();
         } catch (ex) {
             ctx.process_exception(ex);
         }
@@ -255,14 +412,52 @@ client.on('error', function(error_frame) {
     logger.error(msg);
 });
 
+function waitForComplete() {
+    if (messages.length === 0) {
+        logger.info("No messages outstanding...disconnecting");
+        client.disconnect();
+    } else {
+        logger.info("Messages outstanding: " + messages.length);
+        setTimeout(waitForComplete, 1000);
+    }
+}
+
 process.on('SIGINT', function() {
-    logger.info("SIGINT received: disconnecting...");
-    client.unsubscribe({id: 1, destination: pref.queue});
-    client.disconnect();
+    if (process_state === "RUNNING") {
+        process_state = "SHUTTING DOWN";
+        logger.info("SIGINT received: shutting down...");
+        client.unsubscribe({id: 1, destination: pref.queue});
+        var s3_aborts = 0;
+        var index_aborts = 0;
+        var unindex_aborts = 0;
+        for (var i=0; i<messages.length; i++) {
+            var ctx = messages[i];
+            if (ctx.s3_request) {
+                s3_aborts++;
+                ctx.s3_request.abort();
+                ctx.s3_request = null;
+            } else if (ctx.index_req) {
+                index_aborts++;
+                ctx.index_req.abort();
+                ctx.index_req = null;
+            } else if (ctx.unindex_req) {
+                unindex_aborts++;
+                ctx.unindex_req.abort();
+                ctx.unindex_req = null;
+            }
+        }
+        if ((s3_aborts + index_aborts) > 0) {
+            logger.info("Aborted " + s3_aborts + " s3, " + index_aborts + " index, and " + unindex_aborts + " unindex requests.");
+        }
+        waitForComplete();
+    } else {
+        logger.info("SIGINT received a second time: aborting...");
+        client.disconnect();
+    }
 });
 
 client.on('disconnected', function(err) {
-    var msg = "finished with " + messages_outstanding_count + " messages outstanding.";
+    var msg = "finished with " + messages.length + " messages outstanding.";
     logger.info(msg, function() {
         process.exit(1);
     });
